96ab89365c381a1f2b9305cda98ffd7799aa2c7d
ms12297 <ms12297@nyu.edu>
2022-11-08 03:07:54 -0500

    changes

--- back-end/app.js
+++ back-end/app.js
@@ -4,60 +4,6 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 const { v4: uuidv4 } = require("uuid");
 const mystery = "https://github.com/pittcsc/Summer2023-Internships";
-const profLink = "https://app.joinhandshake.com/stu/users/26073328"
-
-async function scrapeProf(url) {
-  let fields = [];
-  try {
-    const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
-    const elemSelector =
-      "style__flex___fCvpa > style__media-body___MV2ef";
-    $(elemSelector).each((parentIdx, parentElem) => {
-      const orgElem = $(parentElem).children()[0];
-      const titleElem = $(parentElem).children()[1];
-      const dateElem = $(parentElem).children()[2];
-      const textElem = $(parentElem).children()[3];
-
-      const companyNameElem = $(parentElem).children()[0];
-      const locationsElem = $(parentElem).children()[1];
-      const positionsElem = $(parentElem).children()[2];
-
-      let profData = {};
-      profData["companyName"] = $(companyNameElem).text();
-      if ($(companyNameElem).children().length > 0) {
-        profData["defaultPositionUrl"] = $(companyNameElem)
-          .find("a")
-          .attr("href");
-      }
-      profData["locations"] = $(locationsElem).text();
-      profData["positions"] = [];
-      $(positionsElem)
-        .contents()
-        .each((posIdx, posElem) => {
-          if (posElem.type === "text") {
-            profData["positions"].push({
-              id: uuidv4(),
-              title: $(posElem).text(),
-              url: profData["defaultPositionUrl"],
-            });
-          } else if (posElem.type === "tag" && posElem.name === "a") {
-            profData["positions"].push({
-              id: uuidv4(),
-              title: $(posElem).text(),
-              url: posElem.attribs["href"],
-            });
-          }
-        });
-      fields.push(profData);
-
-    });
-  } catch (error) {
-    return fields;
-  }
-  return fields;
-
-}
 
 async function scrape(url) {
   let companiesWithPositions = [];
@@ -116,16 +62,7 @@ app.use((req, res, next) => {
 
 app.get("/get_companies", async (req, res) => {
   let companiesToPositions = await scrape(mystery);
-  let companies = [];
-  companiesToPositions.map((company) => {
-      let company = {
-        companyName: company.companyName,
-        url: position.url,
-        locations: company.locations,
-      };
-      companies.push(company);
-    });
-  res.send(companies);
+  res.send(companiesToPositions);
 });
 
 app.get("/get_internships", async (req, res) => {
@@ -145,20 +82,6 @@ app.get("/get_internships", async (req, res) => {
   });
   // console.log(companiesToPositions);
   res.send(internships);
-});
-
-app.get("/get_profile", async (req, res) => {
-  let companiesToPositions = await scrape(mystery);
-  let companies = [];
-  companiesToPositions.map((company) => {
-      let company = {
-        companyName: company.companyName,
-        url: position.url,
-        locations: company.locations,
-      };
-      companies.push(company);
-    });
-  res.send(companies);
 });
 
 module.exports = app;
