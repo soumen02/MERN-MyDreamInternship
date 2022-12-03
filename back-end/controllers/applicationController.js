496140f3df8c9a66be9801e6a56e2ccca00eecbc
ms12297 <ms12297@nyu.edu>
2022-12-03 01:20:49 -0500

    Added functions to edit and retrieve information on notes in the Applications Schema/Collection

--- back-end/controllers/applicationController.js
+++ back-end/controllers/applicationController.js
@@ -1,9 +1,15 @@
 const Application = require("../models/applicationModel");
+const { v4: uuidv4 } = require("uuid");
 
 // get all Applications
 async function getApplications(email) {
     const applications = await Application.find({user:email}).sort({ createdAt: -1 });
     return applications;
+}
+
+async function getSpecApplication(email, id) {
+    const app = await Application.find({user: email, internshipID: id});
+    return app;
 }
 
 // create a new Application
@@ -20,6 +26,7 @@ async function addApplication(application) {
         locations,
         status,
         reviews: "test review",
+        notes: []
     });
     } catch (error) {
     console.log("Error creating new Application", error.message);
@@ -27,7 +34,70 @@ async function addApplication(application) {
     };
 }
 
+//helper function for addNote
+async function notesList(email, id) {
+    const notes = await Application.find({ user: email, internshipID: id});
+    return notes[0].notes;
+}
+
+async function addNote(email, id, entry) {
+    const app = (await getSpecApplication(email, id))[0];
+    const newNote = { id: uuidv4(), title: entry.title, date: entry.date, text: entry.text }
+    try {
+        const updatedApp = await Application.replaceOne({user: email, internshipID: id}, {
+            user: app.user,
+            internshipID: app.internshipID,
+            companyName: app.companyName,
+            companyLogo: app.companyLogo,
+            positionName: app.positionName,
+            locations: app.locations,
+            status: app.status,
+            reviews: app.reviews,
+            notes: [...app.notes, newNote]
+        });
+        return newNote;
+    }
+    catch (error) {
+        console.log("Error updating experience", error.message);
+        console.log("updated experience details: ", exp);
+    }    
+}
+
+async function editNote(email, id, entry) {
+    const notes = await notesList(email, id);
+    
+    const newNotesList = notes.map(note => {
+        if (note.id == entry.id) {
+            return entry;
+        }
+        return note;
+    });
+
+    const app = (await getSpecApplication(email, id))[0];
+
+    try {
+        const updatedApp = await Application.replaceOne({user: email, internshipID: id}, {
+            user: app.user,
+            internshipID: app.internshipID,
+            companyName: app.companyName,
+            companyLogo: app.companyLogo,
+            positionName: app.positionName,
+            locations: app.locations,
+            status: app.status,
+            reviews: app.reviews,
+            notes: newNotesList
+        });
+    }
+    catch (error) {
+        console.log("Error updating experience", error.message);
+        console.log("updated experience details: ", exp);
+    }    
+}
+
 module.exports = {
     getApplications,
     addApplication,
+    notesList,
+    addNote,
+    editNote
 };
