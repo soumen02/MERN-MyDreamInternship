fbd1d98e8dae946be9ae369f9bc691990c90ee64
ms12297 <ms12297@nyu.edu>
2022-12-03 01:16:43 -0500

    Refined InProgress page to include the ApplicationCell components, each ApplicationCell contains backend functionalities for adding Notes and managing the Application

--- front-end/src/components/InProgress.js
+++ front-end/src/components/InProgress.js
@@ -1,9 +1,6 @@
 import { useState, useEffect } from "react";
-import { Container, Stack } from "@mui/system";
+import { Container } from "@mui/system";
 import {
-  Avatar,
-  Card,
-  CardHeader,
   Typography,
   IconButton,
   CssBaseline,
@@ -11,19 +8,15 @@ import {
   Toolbar,
   CircularProgress,
   Grid,
-  CardActionArea,
 } from "@mui/material";
-import { ArrowForward, ArrowBack } from "@material-ui/icons";
+import { ArrowBack } from "@material-ui/icons";
 import useStyles from "./InternshipsStyles";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Footer from "./Footer";
 import { useAuthContext } from "../hooks/useAuthContext";
-import ArticleIcon from "@mui/icons-material/Article";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
-import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 import "./InProgress.css";
-import SeeAll from './SeeAll';
+import ApplicationCell from "./ApplicationCell";
 
 export default function AllApps() {
   const classes = useStyles();
@@ -94,93 +87,6 @@ export default function AllApps() {
   );
 }
 
-function ApplicationCell({ application }) {
-  const classes = useStyles();
-  let path = "/ReviewPage";
-
-  const noteArr = [
-    {
-      title: "Resume1",
-      date: "2021-09-01",
-      text: "Lorem Ipsum"
-    },
-    {
-      title: "Resume2",
-      date: "2021-09-01",
-      text: "Lorem Ipsum"
-    },{
-      title: "Resume3",
-      date: "2021-09-01",
-      text: "Lorem Ipsum"
-    },{
-      title: "Resume4",
-      date: "2021-09-01",
-      text: "Lorem Ipsum"
-    }
-  ]
-
-  return (
-    <Card className={classes.card}>
-      <CardActionArea disableRipple>
-        <CardHeader
-          avatar={
-            <Avatar
-              src={
-                application.companyLogo !== ""
-                  ? application.companyLogo
-                  : "https://source.unsplash.com/random/"
-              }
-            />
-          }
-          action={
-            <Link
-              to={application.internshipID.toString()}
-              state={{ selectedApplication: application }}
-            >
-              <IconButton>
-                <ArrowForward />
-              </IconButton>
-            </Link>
-          }
-          title={application.positionName}
-          subheader={application.companyName}
-        />
-        <Stack direction="row" alignItems="center">
-          <Link
-            style={{ textDecoration: "none" }}
-            to={path}
-            state={{ selectedApplication: application }}
-          >
-            <Stack direction="row" paddingLeft="20px">
-              <IconButton>
-                <Typography paddingRight="5px">Add Note</Typography>
-
-                <AddCircleIcon />
-              </IconButton>
-            </Stack>
-          </Link>
-          <Link
-            style={{ textDecoration: "none" }}
-            to={path}
-            state={{ selectedApplication: application }}
-          >
-            <Stack direction="row" paddingLeft="20px">
-              <IconButton>
-                <Typography paddingRight="5px">Move to Accepted</Typography>
-                <ArrowCircleRightIcon />
-              </IconButton>
-            </Stack>
-          </Link>
-        </Stack>
-        
-        <div className = "contNotes">
-          <SeeAll items={noteArr} state = {true} edit={true} arr = "Notes"/>
-        </div>
-
-      </CardActionArea>
-    </Card>
-  );
-}
 
 function CenteredLoader() {
   return (
