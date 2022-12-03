53b9a463b624e291aa94dba952f59581f9f1908b
zulfkhar00 <olxzulfar@gmail.com>
2022-12-02 23:01:25 -0500

    Reverted Internships page to a stable stage

--- front-end/src/components/Internships.js
+++ front-end/src/components/Internships.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { Container } from "@mui/system";
-import Home from "@mui/icons-material/Home";
 import {
   Avatar,
   Card,
@@ -19,14 +18,11 @@ import useStyles from "./InternshipsStyles";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Footer from "./Footer";
-import SearchBar from "material-ui-search-bar";
-import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function Internships() {
   const classes = useStyles();
   const [loaded, setLoaded] = useState(false);
   const [internships, setInternships] = useState([]);
-  const { user } = useAuthContext();
 
   const fetchInternships = () => {
     // setMessages([])
@@ -47,78 +43,25 @@ export default function Internships() {
       });
   };
 
-  const searchInternships = (searchTerm) => {
-    if (searchTerm === "") {
-      fetchInternships();
-    }
-
-    axios
-      .post("http://localhost:5002/search_internships", {
-        params: {
-          searchTerm: searchTerm,
-        },
-      })
-      .then((response) => {
-        {
-          const internships = response.data;
-          // console.log(internships);
-          setInternships(internships);
-        }
-
-      })
-      .catch((err) => {
-        // catching error
-      })
-      .finally(() => {
-        // the response has been received, so remove the loading icon
-        setLoaded(true);
-      });
-  };
-
   // set up loading data from api when the component first loads
   useEffect(() => {
     // fetch messages this once
     fetchInternships();
-    console.log(user.email);
   }, []);
-
 
   return (
     <>
+      <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
-          <Link to="/dash" style={{ flexDirection: "row-reverse" }}>
+          <Link to="/dash">
             <IconButton>
-              <Home />
+              <ArrowBack />
             </IconButton>
           </Link>
-          <Typography variant="h6" color="inherit" noWrap>
-            MyDreamInternship
-          </Typography>
+          <Typography variant="h4">Internships</Typography>
         </Toolbar>
       </AppBar>
-
-      <Typography
-              variant="h2"
-              align="center"
-              color="textPrimary"
-              gutterBottom
-              paddingTop="20px"
-              paddingBottom="20px"
-            >
-              Internships
-            </Typography>
-
-      <SearchBar
-        placeholder="Search Position"
-        // onChange={() => fetchInternships()}
-        onRequestSearch={(e) => searchInternships(e)}
-        onCancelSearch={() => fetchInternships()}
-        style={{
-          margin: "20px",
-          maxWidth: 800
-        }}
-      />
       <main>
         <div>
           {!loaded && <CenteredLoader />}
@@ -136,9 +79,6 @@ export default function Internships() {
 
 function InternshipCell({ internship }) {
   const classes = useStyles();
-
-  // // console.clear();
-  // console.log(internship.positionName);
 
   return (
     <Card className={classes.card}>
