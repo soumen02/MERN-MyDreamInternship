359f5b5fe59cad637bdeedb2b9b3750caf21cc96
Abraiz01 <78402495+Abraiz01@users.noreply.github.com>
2022-12-11 17:18:19 -0500

    added homepage components

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -8,6 +8,18 @@ import NavBar from "./NavBar";
 import Stack from "@mui/material/Stack";
 import { useEffect } from "react";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { CssBaseline, Grid } from "@mui/material";
+import { Container } from "@mui/system";
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export default function Home() {
   const navigate = useNavigate();
@@ -22,51 +34,39 @@ export default function Home() {
 
   return (
     <>
+      <CssBaseline />
+
       <NavBar />
 
-      <Paper
-        elevation={0}
-        sx={{
-          padding: 20,
-        }}
-      >
-        <Stack spacing={3} justifyContent="center" alignItems="center">
-        <Typography
-            variant="h3"
-            align="center"
-            color="text.primary"
-            paragraph
-          >
-            My Dream Internship
-          </Typography>
-          <Typography
-            // component="h5"
-            variant="h6"
-            align="center"
-            color="text.secondary"
-            width={200}
-            gutterBottom
-          >
-            Work towards your goals today
-          </Typography>
+      <main>
+        <div>
 
-          <Stack direction="row" spacing={5}>
-            <Button 
-              variant="contained" 
-              onClick={() => navigate("/sign-up")}
-            >
-              Join Us
-            </Button>
-            <Button
-              sx={{ backgroundColor: "white" }}
-              variant="outlined"
-              onClick={() => navigate("/log-in")}
-            >
-              Sign In
-            </Button>
-          </Stack>
-        </Stack>
-      </Paper>
+          <Container maxWidth="sm">
+            <Typography variant="h3" align="center" color="textPrimary" gutterBottom>
+              My Dream Internship
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Work towards your goals today
+            </Typography>
+
+            <div>
+
+              <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2} justifyContent="center">
+                  <Grid item >
+                    <Button variant="contained" color="primary">Join Us</Button>
+                  </Grid>
+                  <Grid item >
+                    <Button variant="outlined" color="primary">Sign In</Button>
+                  </Grid>
+                </Grid>
+              </Box>
+            </div>
+
+          </Container>
+        </div>
+      </main>
+
     </>
   );
 }
